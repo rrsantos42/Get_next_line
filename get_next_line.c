0f534d0965212bf898984b5e290e3e73d0e93f54
Ricardo Santos <anaraqsantos@gmail.com>
2022-09-21 06:27:52 +0100

    21/09/22

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,38 @@
 /*   By: rsantos <rsantos@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 19:11:02 by rsantos           #+#    #+#             */
-/*   Updated: 2022/08/23 19:11:26 by rsantos          ###   ########.fr       */
+/*   Updated: 2022/09/21 06:24:34 by rsantos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd)
-{
+#include "get_next_line.h"
+
+char* ft_read(int fd, char* saved, char* buffer){
+	
+	int flag;
+	
+	flag = 1;
+	while (flag != 0)
+	{
+		flag = read(fd, buffer, BUFFER_SIZE);
+		
+	}
+	
+}
+
+
+char* get_next_line(int fd)
+{	
+		char* buffer;
+		static char* saved;
+		char* line;
+		
+
+		if(fd < 0 || BUFFER_SIZE <= 0)
+				return (NULL);
+		buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+		if (!buffer)
+			return(NULL);
+		line = ft_read(fd, saved, buffer);
 	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsantos <rsantos@student.42lisboa.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 18:21:00 by rsantos           #+#    #+#             */
+/*   Updated: 2022/08/30 18:21:02 by rsantos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_h
 #define GET_NEXT_LINE_H
 
 
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+
 char *get_next_line(int fd);
+
+
 
 #endif
